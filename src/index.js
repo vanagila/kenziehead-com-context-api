385cee87bca10a5d1a7a4be5f6ac63aebddf6c91
vanagila <vanagilakedna@gmail.com>
2022-06-10 02:06:03 -0300

    fix: cart provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ProductProvider } from "./providers/Products";
+import { CartProvider } from "./providers/Cart";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductProvider>
-        <App />
-      </ProductProvider>
+      <CartProvider>
+        <ProductProvider>
+          <App />
+        </ProductProvider>
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
